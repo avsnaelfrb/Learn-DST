67d027fb8de0ae248f389ab8b0714d8584292c22
avsnaelfrb <annabhaniy@gmail.com>
2026-02-02 04:24:23 +0800

    docs: manipulation string variable to array with clear line

--- String-Processing/index.ts
+++ String-Processing/index.ts
@@ -7,10 +7,11 @@ Struktur data adalah cara menyimpan data.
 
 const atMethod = text.at(8)
 const charAtMethod = text.charAt(10)
-const sliceMethod = text.slice(18, -1)
-const subStringMethod = text.substring(18, -1)
+const sliceMethod = text.slice(20, -1)
+const subStringMethod = text.substring(1, 18)
 const splitMethod = text.split("\n")
 const joinMethod = splitMethod.join(" ")
+const lines = text.split("\n").map(line => line.trim()).filter(Boolean)
 
 console.log('At method:', atMethod);
 console.log('CharAt method:', charAtMethod);
@@ -18,3 +19,4 @@ console.log('Slice method:', sliceMethod);
 console.log('Sub String method:', subStringMethod);
 console.log('Split method:', splitMethod);
 console.log('Join method:', joinMethod);
+console.log('Lines:', lines);
