4d85e42846a06a39fcc04be706f403201aa7b69f
avsnaelfrb <annabhaniy@gmail.com>
2026-02-02 04:04:37 +0800

    first commit

---
+++ String-Processing/index.js
@@ -0,0 +1,9 @@
+var text = "\nBAB I PENDAHULUAN\n\nAlgoritma adalah langkah-langkah penyelesaian masalah.\nStruktur data adalah cara menyimpan data.\n";
+var atMethod = text.at(8);
+var charAtMethod = text.charAt(10);
+var sliceMethod = text.slice(18, -1);
+var subStringMethod = text.substring(18, -1);
+console.log('At method:', atMethod);
+console.log('CharAt method:', charAtMethod);
+console.log('Slice method:', sliceMethod);
+console.log('Sub String method:', subStringMethod);
