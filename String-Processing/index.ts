d2292d085f2f2a7279270d7e73ef59038e716887
avsnaelfrb <annabhaniy@gmail.com>
2026-02-02 04:18:18 +0800

    docs: join method

--- String-Processing/index.ts
+++ String-Processing/index.ts
@@ -10,9 +10,11 @@ const charAtMethod = text.charAt(10)
 const sliceMethod = text.slice(18, -1)
 const subStringMethod = text.substring(18, -1)
 const splitMethod = text.split("\n")
+const joinMethod = splitMethod.join(" ")
 
 console.log('At method:', atMethod);
 console.log('CharAt method:', charAtMethod);
 console.log('Slice method:', sliceMethod);
 console.log('Sub String method:', subStringMethod);
 console.log('Split method:', splitMethod);
+console.log('Join method:', joinMethod);
