0008f6e31b60b2604412886a82e333e9b69ad4a6
avsnaelfrb <annabhaniy@gmail.com>
2026-02-02 04:04:43 +0800

    first commit

---
+++ String-Processing/index.ts
@@ -0,0 +1,16 @@
+const text: string = `
+BAB I PENDAHULUAN
+
+Algoritma adalah langkah-langkah penyelesaian masalah.
+Struktur data adalah cara menyimpan data.
+`;
+
+const atMethod = text.at(8)
+const charAtMethod = text.charAt(10)
+const sliceMethod = text.slice(18, -1)
+const subStringMethod = text.substring(18, -1)
+
+console.log('At method:', atMethod);
+console.log('CharAt method:', charAtMethod);
+console.log('Slice method:', sliceMethod);
+console.log('Sub String method:', subStringMethod);
