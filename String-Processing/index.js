1c980da04f7ab740de783024becff51536778d36
avsnaelfrb <annabhaniy@gmail.com>
2026-02-02 04:24:14 +0800

    docs: manipulation string variable to array with clear line (js compile)

--- String-Processing/index.js
+++ String-Processing/index.js
@@ -1,13 +1,15 @@
 var text = "\nBAB I PENDAHULUAN\n\nAlgoritma adalah langkah-langkah penyelesaian masalah.\nStruktur data adalah cara menyimpan data.\n";
 var atMethod = text.at(8);
 var charAtMethod = text.charAt(10);
-var sliceMethod = text.slice(18, -1);
-var subStringMethod = text.substring(18, -1);
+var sliceMethod = text.slice(20, -1);
+var subStringMethod = text.substring(1, 18);
 var splitMethod = text.split("\n");
 var joinMethod = splitMethod.join(" ");
+var lines = text.split("\n").map(function (line) { return line.trim(); }).filter(Boolean);
 console.log('At method:', atMethod);
 console.log('CharAt method:', charAtMethod);
 console.log('Slice method:', sliceMethod);
 console.log('Sub String method:', subStringMethod);
 console.log('Split method:', splitMethod);
 console.log('Join method:', joinMethod);
+console.log('Lines:', lines);
