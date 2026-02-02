8fda3f064c28c2587f5af81510a31ac57801ab70
avsnaelfrb <annabhaniy@gmail.com>
2026-02-03 05:00:01 +0800

    docs: data structure with simple node (js compile)

---
+++ Data-Modeling/index.js
@@ -0,0 +1,11 @@
+var node = [
+    {
+        type: 'heading',
+        text: 'BAB I PENDAHULUAN'
+    },
+    {
+        type: 'paragraph',
+        text: 'Algoritma adalah sekumpulan langkah-langkah sistematis yang disusun secara logis untuk menyelesaikan sebuah masalah dalam pemrograman.'
+    }
+];
+console.log('Node DST:', node);
