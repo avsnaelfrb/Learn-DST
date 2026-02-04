be3cf996bd088bf81bf4008da8db9d2bf580be40
avsnaelfrb <annabhaniy@gmail.com>
2026-02-04 23:59:46 +0800

    docs: create function read docs to raw text

--- Read-File-Text/src/index.ts
+++ Read-File-Text/src/index.ts
@@ -1,2 +1,12 @@
 import * as mammoth from 'mammoth';
+import path from 'path';
 
+async function readDocx() {
+    const result = await mammoth.extractRawText({
+        path: path.join(__dirname, '../src/sample.docx')
+    })
+    
+    console.log(result);
+}
+
+readDocx()
