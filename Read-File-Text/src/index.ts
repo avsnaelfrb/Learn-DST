59d75b1a9ada9da465066329d7669a34c88289a1
avsnaelfrb <annabhaniy@gmail.com>
2026-02-12 18:10:44 +0800

    docs: create function to make object from a file

--- Read-File-Text/src/index.ts
+++ Read-File-Text/src/index.ts
@@ -1,12 +1,32 @@
 import * as mammoth from 'mammoth';
 import path from 'path';
 
-async function readDocx() {
-    const result = await mammoth.extractRawText({
-        path: path.join(__dirname, '../src/sample.docx')
+type NodeType = 'heading' | 'paragraph'
+
+async function readDocx(filename: string) {
+    const rawText = await mammoth.extractRawText({
+        path: path.join(__dirname, `../src/${filename}.docx`)
     })
     
+    const cleanText = rawText.value.split('\n').map(l => l.trim()).filter(Boolean)
+    const result = stringToObject(cleanText)
+
     console.log(result);
 }
 
-readDocx()
+function detectType(line: any): NodeType {
+    if (line.startsWith('BAB')) {
+        return 'heading'
+    }
+    return 'paragraph'
+}
+
+function stringToObject(cleanLine: string[]) {
+    return cleanLine.map(line => ({
+        type: detectType(line),
+        text: line
+    }))
+}
+
+
+readDocx("sample")
