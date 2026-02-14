a1712cedbf9a5366fefdae698f5591a435f0d1ee
avsnaelfrb <annabhaniy@gmail.com>
2026-02-14 11:47:07 +0800

    docs: crate normalize function

--- Read-File-Text/src/example2.ts
+++ Read-File-Text/src/example2.ts
@@ -6,9 +6,23 @@ const readDocx = async (filename:string) => {
         path: path.join(__dirname, `../src/${filename}.docx`)
     })
 
-    // const cleanText = rawText.value.split('\n').map(l => l.trim()).filter(Boolean)
+    const cleanText = rawText.value.split('\n').map(l => l.trim()).filter(Boolean)
+    const normalize = normalizeEnum(cleanText)
+    console.log(normalize);
+}
 
-    console.log(rawText);
+const normalizeEnum = (lines: string[]): string[] => {
+    const upperEnumRegex = /([A-Z]\.)/g
+    const lowerEnumRegex = /([a-z]\))/g
+    const delimiter = '|||'
+
+    return lines.flatMap((line) => {
+        const processedLine = line
+            .replace(upperEnumRegex, `${delimiter}$1`)
+            .replace(lowerEnumRegex, `${delimiter}$1`)
+        
+        return processedLine.split(delimiter).map(l => l.trim()).filter(Boolean)
+    })
 }
 
 readDocx('sample-v2.0')
