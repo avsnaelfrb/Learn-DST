122c6a51de24b1a485c008eecb2f9f1a37d86b61
avsnaelfrb <annabhaniy@gmail.com>
2026-02-16 16:04:16 +0800

    docs: function classification

--- Read-File-Text/src/example2.ts
+++ Read-File-Text/src/example2.ts
@@ -1,14 +1,15 @@
 import * as mammoth from 'mammoth'
 import path from 'path'
 
-const readDocx = async (filename:string) => {
+const readDocx = async (filename: string) => {
     const rawText = await mammoth.extractRawText({
         path: path.join(__dirname, `../src/${filename}.docx`)
     })
 
     const cleanText = rawText.value.split('\n').map(l => l.trim()).filter(Boolean)
     const normalize = normalizeEnum(cleanText)
-    console.log(normalize);
+    // console.log(normalize);
+    return normalize;
 }
 
 const normalizeEnum = (lines: string[]): string[] => {
@@ -20,4 +21,43 @@ const normalizeEnum = (lines: string[]): string[] => {
     )
 }
 
-readDocx('sample-v2.0')
+type DetectType = null | 'Heading' | 'List-Item' | 'Container' | 'Paragraph'
+interface NodeObject {
+    type: DetectType
+    level: number
+    text: string
+} 
+
+const classification = async (filename: string) => {
+    const resultNormalize = await readDocx(filename)
+    let currentLevel: number = 0
+
+    for (let i = 0; i < resultNormalize.length; i++) {
+        const text = resultNormalize[i]!.trim();
+
+        if (text!.length === 0) {
+            continue;
+        }
+
+        let detectedType: DetectType = null
+        let detectedLevel = null
+
+        if (text.startsWith('BAB')) {
+            detectedType = 'Heading'
+            detectedLevel = 1
+            currentLevel = 1
+        } else {
+
+            const subbabRegex = /^\d+(?:\.\d+)*\.?\s+/
+            const matchesSubbabPattern = text.match(subbabRegex)
+            if (matchesSubbabPattern) {
+                matchesSubbabPattern[0].trim()
+            } else {
+                
+            }
+        }
+    }
+    // console.log(resultNormalize);
+}
+
+classification('sample-v2.0')
